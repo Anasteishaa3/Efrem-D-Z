c9e39fcbba440c368fae9f81f71b9eae7b3907c7
Anasteishaa3 <nastya.ov.987@gmail.com>
2024-11-26 22:42:05 +0500

    Add files via upload

---
+++ module_3_4.py
@@ -0,0 +1,16 @@
+def single_root_words(root_word, *other_words):
+    same_words = []
+    words = []
+    word = root_word.lower()
+    for w in other_words:
+        w_lower = w.lower()
+        if w_lower.count(word):
+            same_words.append(w)
+        else:
+            words.append(w)
+    return same_words, words
+
+
+same_words, words = single_root_words("тр", "Тропа", "треска", "трава", "друг", "ТРЕК")
+print("Однокоренные:", same_words)
+print("Не однокоренные:", words)
